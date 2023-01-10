96605bf8671cab789e1d777fd066ceeb68a2f1a4
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2023-01-10 16:54:35 -0400

    migrate to typescript

---
+++ scripts/underlying-upgradable-token/upgrade-token.ts
@@ -0,0 +1,22 @@
+import { ethers, upgrades, network } from "hardhat";
+import { networkConfig } from "./helper-hardhat-config";
+
+/**
+ * Upgrades MyUpgradableToken to V2 verion.
+ * Upgrading emits the Upgraded() event, which should trigger
+ * Defender Sentinel if it's running on the same network.
+ */
+async function main() {
+  console.log("Upgrading UpgradableToken contract...");
+  const { proxyAddress } = networkConfig[network.config.chainId!];
+  const TokenV2 = await ethers.getContractFactory("MyUpgradableTokenV2");
+  await upgrades.upgradeProxy(proxyAddress, TokenV2);
+  console.log("Token upgraded!");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
