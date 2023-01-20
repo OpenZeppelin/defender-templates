339197af390aedec1a48f66e2db3a439ea7a7e15
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2023-01-20 16:57:49 -0400

    detect-low-balance: import abi from json

--- defender/auto-fund-chainlink-subscription/autotasks/detect-low-balance/index.js
+++ defender/auto-fund-chainlink-subscription/autotasks/detect-low-balance/index.js
@@ -1,46 +1,11 @@
-import { ethers } from "ethers";
-import { DefenderRelayProvider } from "defender-relay-client/lib/ethers";
-import { vrfCoordinatorAddress, subscriptionId, threshold } from "../../subscription-config.dev.yml";
-const ABI = `[
-    {
-        "inputs": [
-          {
-            "internalType": "uint64",
-            "name": "subId",
-            "type": "uint64"
-          }
-        ],
-        "name": "getSubscription",
-        "outputs": [
-          {
-            "internalType": "uint96",
-            "name": "balance",
-            "type": "uint96"
-          },
-          {
-            "internalType": "uint64",
-            "name": "reqCount",
-            "type": "uint64"
-          },
-          {
-            "internalType": "address",
-            "name": "owner",
-            "type": "address"
-          },
-          {
-            "internalType": "address[]",
-            "name": "consumers",
-            "type": "address[]"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      }
-  ]`;
+import { ethers } from 'ethers';
+import { DefenderRelayProvider } from 'defender-relay-client/lib/ethers';
+import { vrfCoordinatorAddress, subscriptionId, threshold } from '../../subscription-config.dev.yml';
+import ABI from '../../abis/vrfCoordinator.json';
 
 //gets called once per block
 export async function handler(event) {
-  console.log("Starting autotask...");
+  console.log('Starting autotask...');
   const thresholdInWei = ethers.utils.parseEther(threshold);
   const provider = new DefenderRelayProvider(event);
   const payload = event.request.body;
--- defender/auto-fund-chainlink-subscription/rollup.config.mjs
+++ defender/auto-fund-chainlink-subscription/rollup.config.mjs
@@ -1,29 +1,29 @@
-import json from "@rollup/plugin-json";
-import yaml from "@rollup/plugin-yaml";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import builtins from "builtin-modules";
+import json from '@rollup/plugin-json';
+import yaml from '@rollup/plugin-yaml';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import builtins from 'builtin-modules';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = [
   {
-    input: "autotasks/detect-low-balance/index.js",
+    input: 'autotasks/detect-low-balance/index.js',
     output: {
-      file: "autotasks/detect-low-balance/dist/index.js",
-      format: "cjs",
+      file: 'autotasks/detect-low-balance/dist/index.js',
+      format: 'cjs',
     },
-    plugins: [yaml()],
+    plugins: [json({ compact: true }), yaml()],
   },
   {
-    input: "autotasks/fund-subscription/index.js",
+    input: 'autotasks/fund-subscription/index.js',
     output: {
-      file: "autotasks/fund-subscription/dist/index.js",
-      format: "cjs",
+      file: 'autotasks/fund-subscription/dist/index.js',
+      format: 'cjs',
     },
     plugins: [resolve({ preferBuiltins: true }), commonjs(), json({ compact: true }), yaml()],
-    external: [...builtins, "ethers", "web3", "axios", /^defender-relay-client(\/.*)?$/],
+    external: [...builtins, 'ethers', 'web3', 'axios', /^defender-relay-client(\/.*)?$/],
   },
 ];
 
