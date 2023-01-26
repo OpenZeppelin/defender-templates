0498a81a8ffbb3c3a2ddfb33be2f786291ae3f8e
Tim Pechersky <tim.pechersky@openzeppelin.com>
2023-01-26 12:31:02 +0800

    test failed with import not sure why

--- scripts/libraries/ERC721.ts
+++ scripts/libraries/ERC721.ts
@@ -1,7 +1,7 @@
 // import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { ethers } from 'ethers';
 import { ERC721Mock } from '../../types/typechain';
-import Erc721Abi from '../../abi/contracts/ERC721Mock.sol/ERC721Mock.json';
+const Erc721Abi = require('../../abi/contracts/ERC721Mock.sol/ERC721Mock.json');
 export const transferToken = async ({
   tokenAddress,
   tokenId,
