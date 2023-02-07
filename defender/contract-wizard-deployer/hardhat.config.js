577a92307aaaeebd58d3eddb0022e7518930a1ff
Nomad-AE <nomad@arbitraryexecution.com>
2023-02-07 22:08:20 +0800

    AED-764: fully working deployment

--- defender/contract-wizard-deployer/hardhat.config.js
+++ defender/contract-wizard-deployer/hardhat.config.js
@@ -10,9 +10,9 @@ const { AdminClient } = require('defender-admin-client');
 require('dotenv').config();
 
 // eslint-disable-next-line no-undef
-subtask('verifySecrets', 'Validates all stored secrets and returns a provider and signer')
+subtask('verifySecrets', 'Validates all stored secrets and returns signer')
   .addOptionalVariadicPositionalParam('taskArgs', 'task arguments')
-  .setAction(async (taskArgs) => {
+  .setAction(async (taskArgs, hre) => {
     // Set default stage
     const { stage = 'dev' } = taskArgs;
     // Deployment requires Defender Admin and Relay
@@ -61,26 +61,26 @@ subtask('verifySecrets', 'Validates all stored secrets and returns a provider an
       const signer = new DefenderRelaySigner(credentials, provider, {
         speed: 'fast',
       });
-      let relayAddress;
-      let relayNetwork;
+      let signerAddress;
+      let signerNetwork;
       try {
-        relayAddress = await signer.getAddress();
+        signerAddress = await signer.getAddress();
         const chainId = await signer.getChainId();
         const chainIdMap = {
           1: 'mainnet',
           5: 'goerli',
         };
-        relayNetwork = chainIdMap[chainId];
+        signerNetwork = chainIdMap[chainId];
       } catch (error) {
         throw new Error(`Issue with Defender Relay: ${error}`);
       }
-      console.log(`Defender Relay is on ${relayNetwork} using address: ${relayAddress}`);
+      console.log(`Defender Relay is on ${signerNetwork} using address: ${signerAddress}`);
 
       // Test Etherscan
       let relayBalance;
-      const api = etherscanApi.init(etherscanApiKey, relayNetwork);
+      const api = etherscanApi.init(etherscanApiKey, signerNetwork);
       try {
-        ({ result: relayBalance } = await api.account.balance(relayAddress));
+        ({ result: relayBalance } = await api.account.balance(signerAddress));
       } catch (error) {
         throw new Error(`Issue with Etherscan: ${error}`);
       }
@@ -90,9 +90,13 @@ subtask('verifySecrets', 'Validates all stored secrets and returns a provider an
         throw new Error('Relay balance is zero, add some funds and try again');
       }
 
-      return { provider, signer, relayNetwork };
+      return { signer, signerAddress, signerNetwork };
     }
-    return {};
+
+    // If simulate flag is enabled
+    const [signer] = await hre.ethers.getSigners();
+    const signerAddress = await signer.getAddress();
+    return { signer, signerAddress, signerNetwork: 'hardhat' };
   });
 
 // eslint-disable-next-line no-undef
@@ -105,28 +109,75 @@ task('contract', 'Deploys contract')
   .setAction(async (taskArgs, hre) => {
     // Validate secrets and retrieve a provider and signer
     const {
-      provider = hre.provider,
-      // signer = (await hre.ethers.getsigners),
-      network = 'hardhat',
+      signer,
+      signerNetwork,
     } = await hre.run('verifySecrets', taskArgs);
 
-
-    console.log(hre.ethers.getSigners());
     const contractFactory = await hre.ethers.getContractFactory(taskArgs.contractName);
-    console.log(`Deploying ${taskArgs.contractName} to ${network} network`);
-
-    // let contract;
-    // if (taskArgs.simulate === true) {
-    //   const contract = await contractFactory.deploy();
-    // } else {
+    console.log(`Deploying ${taskArgs.contractName} to ${signerNetwork} network`);
     const contract = await contractFactory.connect(signer).deploy();
-    // }
-    console.log(`Deployed contract to ${contract.address} on ${network} network`);
-
-    // const contract = await contractFactory.deploy();
-    // await contract.deployed();
-    // console.log(contract.address);
+    console.log(`Deployed contract to ${contract.address} on ${signerNetwork} network`);
     console.log('Done');
+    return contract;
+  });
+
+// eslint-disable-next-line no-undef
+task('governance', 'Deploys Token, Timelock and Governor contracts with Defender Relay as the admin and owner')
+  .addParam('tokenName', 'Token contract name')
+  .addParam('timelockName', 'Timelock contract name')
+  .addParam('governorName', 'Governor contract name')
+  .addOptionalParam('stage', 'Deployment stage (uses dev by default)')
+  .addFlag('simulate', 'Only simulates the blockchain effects')
+  .addFlag('verify', 'Verifies contract on Etherscan')
+  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Validate secrets and retrieve a provider and signer
+    const {
+      signer,
+      signerNetwork,
+      signerAddress,
+    } = await hre.run('verifySecrets', taskArgs);
+
+    // Compile contracts
+    const tokenFactory = await hre.ethers.getContractFactory(taskArgs.tokenName);
+    const timelockFactory = await hre.ethers.getContractFactory(taskArgs.timelockName);
+    const governorFactory = await hre.ethers.getContractFactory(taskArgs.governorName);
+
+    // --------
+    // Token contract deployment
+    // --------
+    console.log(`Deploying ${taskArgs.tokenName} to ${signerNetwork} network`);
+    const token = await tokenFactory.connect(signer).deploy();
+    await token.deployed();
+    console.log(`Deployed token to ${token.address} on ${signerNetwork} network`);
+
+    // --------
+    // Timelock contract deployment
+    // expect arguments: uint256 minDelay, address[] proposers, address[] executors, address admin
+    // --------
+    const minDelay = ethers.BigNumber.from(0);
+    // Proposers and executors must be an array of addresses
+    // use the same addresses used for the proposers, executors and admin
+    const proposers = [signerAddress];
+    const executors = [signerAddress];
+    const admin = signerAddress;
+
+    // deploy the DemoTimelock contract
+    console.log(`Deploying ${taskArgs.timelockName} to ${signerNetwork} network`);
+    const timelock = await timelockFactory.connect(signer)
+      .deploy(minDelay, proposers, executors, admin);
+    await timelock.deployed();
+    console.log(`Deployed timelock to ${timelock.address} on ${signerNetwork} network`);
+
+    // --------
+    // Governor contract deployment
+    // expects arguments: address token, address timelock
+    // --------
+    console.log(`Deploying ${taskArgs.governorName} to ${signerNetwork} network`);
+    const governor = await governorFactory.connect(signer)
+      .deploy(token.address, timelock.address);
+    await governor.deployed();
+    console.log(`Deployed governor to ${governor.address} on ${signerNetwork} network`);
   });
 
 // eslint-disable-next-line no-undef
@@ -136,7 +187,7 @@ task('verify-etherscan', 'Verify deployed contract on Etherscan')
     try {
       await hre.run('verify:verify', {
         address: contractAddress,
-        contract: 'contracts/NFT.sol:NFT' // <path-to-contract>:<contract-name>
+        contract: 'contracts/NFT.sol:NFT', // <path-to-contract>:<contract-name>
       });
     } catch ({ message }) {
       console.error(message);
