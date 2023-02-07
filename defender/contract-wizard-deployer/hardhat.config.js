05c00624c80597d668e190f01f547017db747b31
Nomad-AE <nomad@arbitraryexecution.com>
2023-02-07 20:18:30 +0800

    AED-764: added more error handling

--- defender/contract-wizard-deployer/hardhat.config.js
+++ defender/contract-wizard-deployer/hardhat.config.js
@@ -1,38 +1,146 @@
+/* eslint-disable import/no-extraneous-dependencies */
 require('@nomicfoundation/hardhat-toolbox');
 require('@nomiclabs/hardhat-etherscan');
 
+const etherscanApi = require('etherscan-api');
+const req = require('require-yml');
+const { ethers } = require('ethers');
 const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
 const { AdminClient } = require('defender-admin-client');
 require('dotenv').config();
 
-task('contract', 'Deploys contract')
-  .addParam('contractName', 'contract name')
-  .addFlag('simulate', 'only simulates the blockchain effects')
-  .addFlag('verify', 'Verifies contract on Etherscan')
-  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
-  .setAction(async (taskArgs, hre) => {
-    const contractFactory = await hre.ethers.getContractFactory(taskArgs.contractName);
-    if (!taskArgs.simulate) {
-      const relayerApiKey = process.env.RELAYER_API_KEY;
-      if (relayerApiKey === undefined) {
-        throw new Error('Could not find Relay API key in the environment secrets');
+// eslint-disable-next-line no-undef
+subtask('verifySecrets', 'Validates all stored secrets and returns a provider and signer')
+  .addOptionalVariadicPositionalParam('taskArgs', 'task arguments')
+  .setAction(async (taskArgs) => {
+    // Set default stage
+    const { stage = 'dev' } = taskArgs;
+    // Deployment requires Defender Admin and Relay
+    if (taskArgs.simulate === false) {
+      const secrets = req(`../.secrets/${stage}.yml`);
+      const {
+        'defender-api-key': defenderApiKey,
+        'defender-api-secret': defenderSecretKey,
+        'relay-api-key': relayApiKey,
+        'relay-api-secret': relaySecretKey,
+        'etherscan-api-key': etherscanApiKey,
+      } = secrets.keys;
+
+      if (defenderApiKey === undefined) {
+        throw new Error('Could not find Defender API key in defender/.secrets/<stage>.yml');
       }
-      const relayerApiSecret = process.env.RELAYER_SECRET_KEY;
-      if (relayerApiSecret === undefined) {
-        throw new Error('Could not find Relay Secret Key in the environment secrets');
+      if (defenderSecretKey === undefined) {
+        throw new Error('Could not find Defender Secret Key in defender/.secrets/<stage>.yml');
+      }
+      if (relayApiKey === undefined) {
+        throw new Error('Could not find Defender API key in defender/.secrets/<stage>.yml');
+      }
+      if (relaySecretKey === undefined) {
+        throw new Error('Could not find Defender Secret Key in defender/.secrets/<stage>.yml');
+      }
+      if (etherscanApiKey === undefined) {
+        throw new Error('Could not find Etherscan API Key in defender/.secrets/<stage>.yml');
       }
 
-      const credentials = { apiKey: relayerApiKey, apiSecret: relayerApiSecret };
+      // Test Defender Admin
+      const client = new AdminClient({
+        apiKey: defenderApiKey,
+        apiSecret: defenderSecretKey,
+      });
+
+      try {
+        await client.listContracts();
+      } catch (error) {
+        throw new Error(`Issue with Defender Admin: ${error}`);
+      }
+      console.log('Defender Admin is successfully connected');
+
+      // Test Defender Relay
+      const credentials = { apiKey: relayApiKey, apiSecret: relaySecretKey };
       const provider = new DefenderRelayProvider(credentials);
       const signer = new DefenderRelaySigner(credentials, provider, {
         speed: 'fast',
       });
+      let relayAddress;
+      let relayNetwork;
+      try {
+        relayAddress = await signer.getAddress();
+        const chainId = await signer.getChainId();
+        const chainIdMap = {
+          1: 'mainnet',
+          5: 'goerli',
+        };
+        relayNetwork = chainIdMap[chainId];
+      } catch (error) {
+        throw new Error(`Issue with Defender Relay: ${error}`);
+      }
+      console.log(`Defender Relay is on ${relayNetwork} using address: ${relayAddress}`);
 
-      await contractFactory.connect(signer);
+      // Test Etherscan
+      let relayBalance;
+      const api = etherscanApi.init(etherscanApiKey, relayNetwork);
+      try {
+        ({ result: relayBalance } = await api.account.balance(relayAddress));
+      } catch (error) {
+        throw new Error(`Issue with Etherscan: ${error}`);
+      }
+      console.log('Etherscan is working');
+      relayBalance = ethers.BigNumber.from(relayBalance);
+      if (relayBalance.eq(0)) {
+        throw new Error('Relay balance is zero, add some funds and try again');
+      }
+
+      return { provider, signer, relayNetwork };
     }
-    const contract = await contractFactory.deploy();
-    await contract.deployed();
-    console.log(contract.address);
+    return {};
+  });
+
+// eslint-disable-next-line no-undef
+task('contract', 'Deploys contract')
+  .addParam('contractName', 'Contract name')
+  .addOptionalParam('stage', 'Deployment stage (uses dev by default)')
+  .addFlag('simulate', 'Only simulates the blockchain effects')
+  .addFlag('verify', 'Verifies contract on Etherscan')
+  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Validate secrets and retrieve a provider and signer
+    const {
+      provider = hre.provider,
+      // signer = (await hre.ethers.getsigners),
+      network = 'hardhat',
+    } = await hre.run('verifySecrets', taskArgs);
+
+
+    console.log(hre.ethers.getSigners());
+    const contractFactory = await hre.ethers.getContractFactory(taskArgs.contractName);
+    console.log(`Deploying ${taskArgs.contractName} to ${network} network`);
+
+    // let contract;
+    // if (taskArgs.simulate === true) {
+    //   const contract = await contractFactory.deploy();
+    // } else {
+    const contract = await contractFactory.connect(signer).deploy();
+    // }
+    console.log(`Deployed contract to ${contract.address} on ${network} network`);
+
+    // const contract = await contractFactory.deploy();
+    // await contract.deployed();
+    // console.log(contract.address);
+    console.log('Done');
+  });
+
+// eslint-disable-next-line no-undef
+task('verify-etherscan', 'Verify deployed contract on Etherscan')
+  .addParam('contractAddress', 'Contract address deployed')
+  .setAction(async (contractAddress, hre) => {
+    try {
+      await hre.run('verify:verify', {
+        address: contractAddress,
+        contract: 'contracts/NFT.sol:NFT' // <path-to-contract>:<contract-name>
+      });
+    } catch ({ message }) {
+      console.error(message);
+    }
   });
 
 /** @type import('hardhat/config').HardhatUserConfig */
