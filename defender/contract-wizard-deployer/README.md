e153f3a7e9da41b8a8fc1517bdef76422f8f47ff
arbitrary-nomad <nomad@arbitraryexecution.com>
2023-02-12 23:56:22 +0800

    AED-764: Deploy contracts with relays (#20)

---
+++ defender/contract-wizard-deployer/README.md
@@ -0,0 +1,158 @@
+# Contract Wizard Deployer
+
+This Hardhat script enables developers to take Solidity files that are downloaded from OpenZeppelin's [Contract Wizard](https://wizard.openzeppelin.com/), deploy them using a [Defender Relay](https://defender.openzeppelin.com/#/relay), and upload them to [Defender Admin](https://defender.openzeppelin.com/#/admin).
+
+## Setup
+
+### Defender Account Setup
+
+- In your [Defender account](https://defender.openzeppelin.com/), select the Hamburger icon in the upper right corner and click on **Team API Keys**
+- In the Team API Keys screen, click **Create API Key**
+- Make sure that the options for **Manage Proposals and Contracts** is selected (we do not make use of the **Manage Relayers**, **Manage Autotasks**, and **Manage Sentinels** options with this template)
+- Click **Save**
+- Copy your API key and Secret key to a local file (you will **NOT** be able to view your Secret key again after this message box goes away)
+- Make sure that you really did copy your API key and Secret key to a local file
+- Check the box for **I’ve written down the Secret key** and select **Close**
+
+### Relay API Keys
+
+(Currently, only Ethereum Mainnet and Goerli Testnet networks are supported by this script)
+
+- In your [Defender account](https://defender.openzeppelin.com/), select the **◎ Relay** link on the upper left side to get to the Relay Dashboard
+- If you do not have a Relay on the network that you want to deploy to, create one:
+  - In the Relay Dashboard screen, click **Create Relayer** in the upper right corner
+  - Pick a meaningful name, choose the network that you want to deploy to, and click **Create**
+- Click on the the existing Relay that you would like to use for deployments
+- Click on the settings gear/cog **⚙** at the top middle and choose **+ Create new API key**
+- Copy your Ethereum Address, API key, and Secret key to a local file (you will **NOT** be able to view your Secret key again after this message box goes away)
+- Make sure that you really did copy your API key and Secret key to a local file
+- Check the box for **I’ve written down the secret key** and select **Close**
+
+### Local Code Setup
+
+In the `defender/contract-wizard-deployer` directory, perform the following steps:
+
+- A secrets file can be created for each stage of production. We will create one for development:
+  - Copy and rename the `sample.secrets.yml` to `.secrets/dev.yml`
+  - Modify the four lines in the `.secrets/dev.yml` file, replacing the portion in the angle brackets `<>` with your Defender API keys and Secret keys, as indicated:
+  - `defender-api-key: <API Key goes here>`
+  - `defender-api-secret: <Secret key goes here>`
+  - `relay-api-key: <Relay API Key goes here>`
+  - `relay-api-secret: <Relay Secret key goes here>`
+- Change directories to the Contract Wizard Deployer
+  - `cd contract-wizard-deployer`
+- Run `yarn install` to install the necessary Node packages
+
+### Solidity Files
+
+Contracts can be created, modified and downloaded from OpenZeppelin's [Contracts Wizard](https://wizard.openzeppelin.com/). Hardhat assumes that the contract name will match the Solidity file name and the directory name. Example: MyContract will exist inside of a file called MyContract.sol, located in a folder called MyContract. These contract folders will be located in the `contract-wizard-deployer/contracts` directory.
+
+```text
+contract-wizard-deployer/
+├── contracts/
+│   ├── MyContract
+│   │   └── MyContract.sol - Contains a contract called 'MyContract'
+│   ├── MyToken
+│   │   └── MyToken.sol - Contains a contract called 'MyToken'
+│   └── MyNFT
+│       └── MyNFT.sol - Contains a contract called 'MyNFT'
+├── defender.hh.config.js
+├── package.json
+└── README.md
+```
+
+## Limitations
+
+- Only supports Ethereum Mainnet and Goerli Testnet networks
+- Structs are not supported as a constructor argument
+
+## General Usage
+
+These scripts use Hardhat tasks. The command "`yarn run deploy`" is an alias for "`yarn hardhat --config defender.hh.config.js`". Either command can be used, but this guide will use `yarn run deploy` from this point on.
+
+Usage: `yarn run deploy <task> <arguments>`
+
+Defender-specific Hardhat tasks:
+
+- contract
+- governance
+- to-defender
+
+To get CLI help for any of the tasks, run `yarn run deploy help <task>` and it will provide a list of required and optional arguments.
+
+**Notes on constructor arguments:**
+
+- Arguments are space-separated (`1 2 3 4`)
+- Arrays must be single-quoted. (`'[1,2]'`)
+- Strings must be double-quoted (`"token"`)
+- Addresses should be treated as strings (`"0x0000"`)
+- Strings in an array will use both quotes (`'[1,"token"]'`)
+- Example of passing 3 arguments (`1 "token" '[1,"token"]'`)
+
+### Deploying an Individual Contract
+
+Deploy any individual contract to Ethereum Mainnet or Goerli Testnet network and add the deployed contracts to Defender Admin.
+
+- `yarn run deploy contract <arguments>` - General usage
+- `yarn run deploy help contract` - View help information in CLI
+
+  Required Arguments:
+
+- `--contract-name <name>` - Contract name (MyToken)
+  Optional Arguments:
+- `--stage <stage>` - Deployment stage `dev` unless otherwise specified (dev)
+- `--simulate` - Deploys locally to a temporary Hardhat network. Useful for testing constructor arguments
+- `<constructor argument1> <constructor argument2>...` - Arguments to pass to the constructor
+
+Examples:
+
+- `yarn run deploy contract --contract-name DemoToken --stage dev --simulate`
+- `yarn run deploy contract --contract-name DemoTimelock --simulate 0 '["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]' '["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"]' "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"`
+- `yarn run deploy contract --contract-name DemoGovernor --simulate "0x5FbDB2315678afecb367f032d93F642f64180aa3" "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"`
+- `yarn run deploy contract --contract-name DemoToken` **--Warning-- This is an actual deployment**
+
+### Deploying a Governance suite ([Token](https://docs.openzeppelin.com/contracts/4.x/governance#token), [Timelock](https://docs.openzeppelin.com/contracts/4.x/governance#timelock) and [Governor](https://docs.openzeppelin.com/contracts/4.x/governance#governor))
+
+Deploy the OpenZeppelin Governance Suite with the deployer (Defender Relay) set as proposer, executor, admin and owner of the deployed contracts. After deployment, tokens can manually be minted and roles reassigned as desired. All contracts are also added to Defender Admin.
+
+- `yarn run deploy governance <arguments>` - General usage
+- `yarn run deploy help governance` - View help information in CLI
+
+  Required Arguments:
+
+- `--token-name <name>` - Token contract name (DemoToken)
+- `--timelock-name <name>` - Timelock contract name (DemoTimelock)
+- `--governor-name <name>` - Governor contract name (DemoGovernor)
+  Optional Arguments:
+- `--stage <stage>` - Deployment stage `dev` unless otherwise specified (dev)
+- `--simulate` - Deploys locally to a temporary Hardhat network. Useful for testing constructor arguments
+
+Examples:
+
+- `yarn run deploy governance --timelock-name DemoTimelock --token-name DemoToken --governor-name DemoGovernor --simulate`
+
+### Adding Previously Deployed Contracts to Defender Admin
+
+Any previously deployed contracts can be added to Defender admin with the task `to-defender`.
+
+- `yarn run deploy to-defender <arguments>` - General usage
+- `yarn run deploy help to-defender` - View help information in CLI
+
+  Required Arguments:
+
+- `--contract-name <name>` - Contract name (DemoToken)
+- `--contract-address <name>` - Contract address (0x5FbDB2315678afecb367f032d93F642f64180aa3)
+- `--contract-network <name>` - Contract network (goerli)
+  Optional Arguments:
+- `--stage <stage>` - Deployment stage `dev` unless otherwise specified (dev)
+- `<constructor argument1> <constructor argument2>...` - Arguments to pass to the constructor
+
+Examples:
+
+- `yarn run deploy to-defender --contract-network goerli --contract-name DemoToken --contract-address "0xa2e87B88D805222bf950f81601f43e794a73F481"`
+- `yarn run deploy to-defender --contract-network goerli --contract-name DemoTimelock --contract-address "0xACC0b2A0Ee8445983a8EDA6294c1660C6C0Aa330" 0 '["0x5b46d575f4a5302250233dbbf456d15e6353b7bd"]' '["0x5b46d575f4a5302250233dbbf456d15e6353b7bd"]' "0x5b46d575f4a5302250233dbbf456d15e6353b7bd"`
+- `yarn run deploy to-defender --contract-network goerli --contract-name DemoToken --contract-address "0xEa6CdeD4c27892528C144554624bc28A4da6Ac5C" "0xa2e87B88D805222bf950f81601f43e794a73F481" "0xACC0b2A0Ee8445983a8EDA6294c1660C6C0Aa330"`
+
+## TODO:
+
+- Test and enable Etherscan-Verify
---
+++ defender/contract-wizard-deployer/defender.hh.config.js
@@ -0,0 +1,330 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('@nomicfoundation/hardhat-toolbox');
+
+const req = require('require-yml');
+const { ethers } = require('ethers');
+const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
+const { AdminClient } = require('defender-admin-client');
+const { task } = require('hardhat/config');
+
+// eslint-disable-next-line object-curly-newline
+async function addContractToDefenderAdmin({ contract, name, client, network, address }) {
+  let result;
+  const contractInfo = {
+    abi: JSON.stringify(contract.interface.format(ethers.utils.FormatTypes.json)),
+    name,
+    network,
+    address,
+  };
+  try {
+    result = await client.addContract(contractInfo);
+  } catch (error) {
+    console.error(`Error adding contract ${name} to Defender Admin: ${error}`);
+    throw error;
+  }
+  return result;
+}
+
+function formatArgs(inputArgs) {
+  let args = [];
+  if (inputArgs) {
+    args = inputArgs.map((arg) => {
+      try {
+        // Parse numbers and arrays from inputs. Double quoted inputs will be treated as strings
+        // 1 = number 1
+        // '1' = number 1
+        // "1" = string '1'
+        // '[ 123, "0x0000" ]' will be passed as an array containing a number and a string.
+        return JSON.parse(arg);
+      } catch (error) {
+        return arg;
+      }
+    });
+  }
+  return args;
+}
+
+// eslint-disable-next-line no-undef
+subtask('verifySecrets', 'Validates all stored secrets and returns signer')
+  .addOptionalVariadicPositionalParam('taskArgs', 'task arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Set default stage
+    const { stage = 'dev' } = taskArgs;
+
+    // Deployment requires Defender Admin and Relay
+    if (taskArgs.simulate !== true) {
+      const secrets = req(`../.secrets/${stage}.yml`);
+      const {
+        'defender-api-key': defenderApiKey,
+        'defender-api-secret': defenderSecretKey,
+        'relay-api-key': relayApiKey,
+        'relay-api-secret': relaySecretKey,
+      } = secrets.keys;
+
+      if (defenderApiKey === undefined) {
+        throw new Error('Could not find Defender API key in defender/.secrets/<stage>.yml');
+      }
+      if (defenderSecretKey === undefined) {
+        throw new Error('Could not find Defender Secret key in defender/.secrets/<stage>.yml');
+      }
+      if (relayApiKey === undefined) {
+        throw new Error('Could not find Relay API key in defender/.secrets/<stage>.yml');
+      }
+      if (relaySecretKey === undefined) {
+        throw new Error('Could not find Relay Secret key in defender/.secrets/<stage>.yml');
+      }
+
+      // Test Defender Admin
+      const adminClient = new AdminClient({
+        apiKey: defenderApiKey,
+        apiSecret: defenderSecretKey,
+      });
+
+      try {
+        await adminClient.listContracts();
+      } catch (error) {
+        throw new Error(`Issue with Defender Admin: ${error}`);
+      }
+      console.log('Defender Admin is successfully connected');
+
+      // Test Defender Relay
+      const credentials = { apiKey: relayApiKey, apiSecret: relaySecretKey };
+      const provider = new DefenderRelayProvider(credentials);
+      const signer = new DefenderRelaySigner(credentials, provider, {
+        speed: 'fast',
+      });
+      let signerAddress;
+      let signerNetwork;
+      try {
+        signerAddress = await signer.getAddress();
+        const chainId = await signer.getChainId();
+        const chainIdMap = {
+          1: 'mainnet',
+          5: 'goerli',
+        };
+        signerNetwork = chainIdMap[chainId];
+      } catch (error) {
+        throw new Error(`Issue with Defender Relay: ${error}`);
+      }
+      console.log(`Defender Relay is on ${signerNetwork} using address: ${signerAddress}`);
+
+      return {
+        signer, signerAddress, signerNetwork, adminClient,
+      };
+    }
+
+    // If simulate flag is enabled
+    const [signer] = await hre.ethers.getSigners();
+    const signerAddress = await signer.getAddress();
+    return { signer, signerAddress, signerNetwork: 'hardhat' };
+  });
+
+// eslint-disable-next-line no-undef
+task('contract', 'Deploys contract')
+  .addParam('contractName', 'Contract name')
+  .addOptionalParam('stage', 'Deployment stage (uses dev by default)')
+  .addFlag('simulate', 'Only simulates the blockchain effects')
+  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Validate secrets and retrieve a provider and signer
+    const {
+      adminClient,
+      signer,
+      signerNetwork,
+    } = await hre.run('verifySecrets', taskArgs);
+
+    // Compile all contracts
+    await hre.run('compile');
+
+    const args = formatArgs(taskArgs.constructorArgs);
+
+    const contractFactory = await hre.ethers.getContractFactory(taskArgs.contractName);
+    console.log(`Deploying ${taskArgs.contractName} to ${signerNetwork} network`);
+    if (args.length > 0) {
+      console.log(`with arguments ${JSON.stringify(args)}`);
+    }
+    const contract = await contractFactory.connect(signer).deploy(...args);
+    console.log(`Deployed contract to ${contract.address} on ${signerNetwork} network`);
+
+    if (taskArgs.simulate === false) {
+      // Add contract to Defender
+      const contractObject = {
+        contract,
+        name: taskArgs.contractName,
+        client: adminClient,
+        network: signerNetwork,
+        address: contract.address,
+      };
+      await addContractToDefenderAdmin(contractObject);
+      console.log(`Added ${contractObject.name} to Defender`);
+    }
+  });
+
+// eslint-disable-next-line no-undef
+task('governance', 'Deploys Token, Timelock and Governor contracts with Defender Relay as the admin and owner')
+  .addParam('tokenName', 'Token contract name')
+  .addParam('timelockName', 'Timelock contract name')
+  .addParam('governorName', 'Governor contract name')
+  .addOptionalParam('stage', 'Deployment stage (uses dev by default)')
+  .addFlag('simulate', 'Only simulates the blockchain effects')
+  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Validate secrets and retrieve a provider and signer
+    const {
+      adminClient,
+      signer,
+      signerNetwork,
+      signerAddress,
+    } = await hre.run('verifySecrets', taskArgs);
+
+    // Compile all contracts
+    await hre.run('compile');
+
+    const tokenFactory = await hre.ethers.getContractFactory(taskArgs.tokenName);
+    const timelockFactory = await hre.ethers.getContractFactory(taskArgs.timelockName);
+    const governorFactory = await hre.ethers.getContractFactory(taskArgs.governorName);
+
+    // --------
+    // Token contract deployment
+    // --------
+    const tokenArgs = [];
+
+    console.log(`Deploying ${taskArgs.tokenName} to ${signerNetwork} network`);
+    if (tokenArgs.length > 0) {
+      console.log(`with arguments ${JSON.stringify(tokenArgs)}`);
+    }
+    const token = await tokenFactory.connect(signer).deploy(...tokenArgs);
+    await token.deployed();
+    console.log(`Successfully deployed token to ${token.address} on ${signerNetwork} network`);
+
+    if (taskArgs.simulate === false) {
+      // Add token to Defender
+      const tokenObject = {
+        contract: token,
+        name: taskArgs.tokenName,
+        client: adminClient,
+        network: signerNetwork,
+        address: token.address,
+      };
+      await addContractToDefenderAdmin(tokenObject);
+      console.log(`Added ${tokenObject.name} to Defender`);
+    }
+
+    // --------
+    // Timelock contract deployment
+    // expect arguments: uint256 minDelay, address[] proposers, address[] executors, address admin
+    // --------
+    const minDelay = ethers.BigNumber.from(0);
+    // Proposers and executors must be an array of addresses
+    // use the same addresses used for the proposers, executors and admin
+    const proposers = [signerAddress];
+    const executors = [signerAddress];
+    const admin = signerAddress;
+
+    // deploy the DemoTimelock contract
+    const timelockArgs = [minDelay, proposers, executors, admin];
+
+    console.log(`Deploying ${taskArgs.timelockName} to ${signerNetwork} network`);
+    if (timelockArgs.length > 0) {
+      console.log(`with arguments ${JSON.stringify(timelockArgs)}`);
+    }
+    const timelock = await timelockFactory.connect(signer).deploy(...timelockArgs);
+    await timelock.deployed();
+    console.log(`Successfully deployed timelock to ${timelock.address} on ${signerNetwork} network`);
+
+    if (taskArgs.simulate === false) {
+      // Add timelock to Defender
+      const timelockObject = {
+        contract: timelock,
+        name: taskArgs.timelockName,
+        client: adminClient,
+        network: signerNetwork,
+        address: timelock.address,
+      };
+      await addContractToDefenderAdmin(timelockObject);
+      console.log(`Added ${timelockObject.name} to Defender`);
+    }
+    // --------
+    // Governor contract deployment
+    // expects arguments: address token, address timelock
+    // --------
+    const governorArgs = [token.address, timelock.address];
+
+    console.log(`Deploying ${taskArgs.governorName} to ${signerNetwork} network`);
+    if (governorArgs.length > 0) {
+      console.log(`with arguments ${JSON.stringify(governorArgs)}`);
+    }
+    const governor = await governorFactory.connect(signer).deploy(...governorArgs);
+    await governor.deployed();
+    console.log(`Successfully deployed governor to ${governor.address} on ${signerNetwork} network`);
+
+    if (taskArgs.simulate === false) {
+      // Add governor to Defender
+      const governorObject = {
+        contract: governor,
+        name: taskArgs.governorName,
+        client: adminClient,
+        network: signerNetwork,
+        address: governor.address,
+      };
+      await addContractToDefenderAdmin(governorObject);
+      console.log(`Added ${governorObject.name} to Defender`);
+    }
+  });
+
+// eslint-disable-next-line no-undef
+task('to-defender', 'Adds specified contract to Defender')
+  .addParam('contractName', 'Contract name')
+  .addParam('contractAddress', 'Address of deployed contract')
+  .addParam('contractNetwork', 'Network that contract is deployed to')
+  .addOptionalParam('stage', 'Deployment stage (uses dev by default)')
+  .addOptionalVariadicPositionalParam('constructorArgs', 'Constructor arguments')
+  .setAction(async (taskArgs, hre) => {
+    // Validate secrets and retrieve a provider and signer
+    const {
+      adminClient: client,
+    } = await hre.run('verifySecrets', taskArgs);
+    const signerNetwork = 'hardhat';
+
+    // Compile all contracts first
+    await hre.run('compile');
+
+    const args = formatArgs(taskArgs.constructorArgs);
+
+    const contractFactory = await hre.ethers.getContractFactory(taskArgs.contractName);
+    console.log('Testing deployment arguments');
+    console.log(`Deploying ${taskArgs.contractName} to ${signerNetwork} network`);
+    if (args.length > 0) {
+      console.log(`with arguments ${JSON.stringify(args)}`);
+    }
+    const contract = await contractFactory.deploy(...args);
+    console.log(`Deployed contract to ${contract.address} on ${signerNetwork} network`);
+
+    const {
+      contractName,
+      contractAddress,
+      contractNetwork,
+    } = taskArgs;
+
+    const result = await addContractToDefenderAdmin({
+      contract,
+      name: contractName,
+      client,
+      network: contractNetwork,
+      address: contractAddress,
+    });
+    console.log(`Successfully added ${contractName} to Defender with contract ID: ${result.contractId}`);
+  });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
