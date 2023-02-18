b5b76b47a9618affd06ebfd93d009d2a25bf3841
Nomad-AE <nomad@arbitraryexecution.com>
2023-02-18 10:52:37 +0800

    AE-2334: finished 99% of refill tokens

--- defender/refill-tokens/autotasks/refill-tokens/index.js
+++ defender/refill-tokens/autotasks/refill-tokens/index.js
@@ -1,22 +1,39 @@
+const stackName = 'poap_minter';
+const tokenTypeSecretName = `${stackName}_TOKEN_TYPE`;
+const nftIdSecretName = `${stackName}_NFT_ID`;
+const tokenAddressSecretName = `${stackName}_TOKEN_ADDRESS`;
+const recipientAddressSecretName = `${stackName}_RECIPIENT_ADDRESS`;
+const recipientMinimumBalanceSecretName = `${stackName}_RECIPIENT_MINIMUM_BALANCE`;
+const recipientTopUpAmountSecretName = `${stackName}_RECIPIENT_TOP_UP_AMOUNT`;
+
 const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
 const ethers = require('ethers');
-// const tokenAddress = ethers.constants.AddressZero;
-const recipientTopUpBalance = '1';
-// const recipientAddress = ethers.constants.AddressZero;
-const recipientTopUpAmount = new ethers.BigNumber.from('3');
 
-import {
-  tokenType, nftId,
-  'token-address' as tokenAddress,
-  'recipient-address' as recipientAddress
-} from '../../config.dev.yml';
-const erc1155StandardAbi = ['function mint(address account, uint256 id, uint256 amount, bytes data)'];
-// ERC1155Mock uses a slightly modified mint function
-const erc1155Abi = require('../../../../abi/contracts/ERC1155Mock.sol/ERC1155Mock.json');
-const erc721Abi = require('../../../../abi/contracts/ERC721Mock.sol/ERC721Mock.json');
-const erc20Abi = require('../../../../abi/contracts/ERC20Mock.sol/ERC20Mock.json');
+const ERC1155AbiStandard = ['function mint(address account, uint256 id, uint256 amount, bytes data)'];
+const ERC1155AbiMock = ['function mint(address to, uint256 id, uint256 amount)'];
+const ERC721Abi = ['function safeMint(address to)'];
+const ERC20Abi = ['function mint(address to, uint256 amount)'];
 
 exports.handler = async function (event) {
+  if (!event) { throw new Error('event undefined'); }
+  const { secrets } = event;
+  if (!secrets) { throw new Error('secrets undefined'); }
+
+  const tokenType = secrets[tokenTypeSecretName];
+  const nftId = ethers.BigNumber
+    .from(secrets[nftIdSecretName] ?? 0);
+  const tokenAddress = ethers.utils
+    .getAddress(secrets[tokenAddressSecretName]);
+  const recipientAddress = ethers.utils
+    .getAddress(secrets[recipientAddressSecretName]);
+  const recipientMinimumBalance = ethers.BigNumber
+    .from(secrets[recipientMinimumBalanceSecretName]);
+  const recipientTopUpAmount = ethers.BigNumber
+    .from(secrets[recipientTopUpAmountSecretName]);
+
+  // ERC1155 Specific data
+  const nftData = 0x00;
+
   if (tokenAddress == ethers.constants.AddressZero) throw new Error('Token address not specified');
   if (recipientAddress == ethers.constants.AddressZero) throw new Error('Refill address not specified');
 
@@ -24,44 +41,55 @@ exports.handler = async function (event) {
   const signer = new DefenderRelaySigner(event, provider, {
     speed: 'fast',
   });
-  console.log('Required balance is:', recipientTopUpAmount.toString());
+  console.log('Required balance is:', recipientMinimumBalance.toString());
   let tx;
-  if (tokenType == TokenTypeEnum.ERC20) {
-    const abi = erc20Abi;
+
+  // Case insensitive matching of ERC20 or ERC-20 etc.
+  const erc20 = /erc-?20/gi;
+  const erc721 = /erc-?721/gi;
+  const erc1155 = /erc-?1155/gi;
+
+  if (tokenType.match(erc20)) {
+    const abi = ERC20Abi;
     const contract = new ethers.Contract(tokenAddress, abi, signer);
     const balance = await contract.balanceOf(recipientAddress);
     console.log('Current balance is:', balance.toString());
-    if (balance.lt(recipientTopUpAmount)) {
-      tx = await contract.mint(recipientAddress, recipientTopUpBalance);
+    if (balance.lt(recipientMinimumBalance)) {
+      tx = await contract.mint(recipientAddress, recipientTopUpAmount);
       console.log('Minted ERC20 tokens with tx:', tx.hash);
     } else {
       console.log('Already full. Do nothing.');
     }
-  }
-  if (tokenType == TokenTypeEnum.ERC1155) {
-    const abi = erc1155Abi;
-    const contract = new ethers.Contract(tokenAddress, abi, signer);
+  } else if (tokenType.match(erc1155)) {
+    const contract = new ethers.Contract(tokenAddress, ERC1155AbiStandard, signer);
+    const contractV2 = new ethers.Contract(tokenAddress, ERC1155AbiMock, signer);
     const balance = await contract.balanceOf(recipientAddress, nftId);
     console.log('Current balance is:', balance.toString());
-    if (balance.lt(recipientTopUpAmount)) {
-      tx = await contract.mint(recipientAddress, nftId, recipientTopUpBalance);
+    if (balance.lt(recipientMinimumBalance)) {
+      try {
+        tx = await contract.mint(recipientAddress, nftId, recipientTopUpAmount, nftData);
+      } catch (error) {
+        console.log(`First attempt failed with error: ${error}`)
+        tx = await contractV2.mint(recipientAddress, nftId, recipientTopUpAmount);
+      }
       console.log('Minted ERC1155 tokens with tx:', tx.hash);
     } else {
       console.log('Already full. Do nothing.');
     }
-  }
-  if (tokenType == TokenTypeEnum.ERC721) {
-    const abi = erc721Abi;
+  } else if (tokenType.match(erc721)) {
+    const abi = ERC721Abi;
     const contract = new ethers.Contract(tokenAddress, abi, signer);
     const balance = await contract.balanceOf(recipientAddress);
     console.log('Current balance is:', balance.toString());
-    if (balance.lt(recipientTopUpAmount)) {
-      for (let i = 0; i < Number(recipientTopUpBalance); i++) {
+    if (balance.lt(recipientMinimumBalance)) {
+      for (let i = 0; i < Number(recipientTopUpAmount); i++) {
         tx = await contract.safeMint(recipientAddress);
         console.log('Minted ERC721 token with tx:', tx.hash);
       }
     } else {
       console.log('Already full. Do nothing.');
     }
+  } else {
+    throw new Error('Token Type is not erc20, erc721, nor erc1155')
   }
 };
