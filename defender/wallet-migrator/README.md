5067452beaf9cfe36fd579809f9e07092aafa19e
darian-ae <darian@arbitraryexecution.com>
2023-02-23 13:23:47 -0800

    refactored approve script

--- defender/wallet-migrator/README.md
+++ defender/wallet-migrator/README.md
@@ -1,3 +1,16 @@
 # Wallet Migrator
 
 This Defender Autotask will automatically transfer funds from a user's wallet address to another specified wallet address.
+
+
+### .env setup
+
+add instructions on how to setup covalent api key
+
+GOERLI_RPC_URL
+PRIVATE_KEY
+COVALENT_API_KEY
+
+### config setup
+
+deploy relayer and autotask first before you run script to approve all tokens
--- defender/wallet-migrator/approveTokens.js
+++ defender/wallet-migrator/approveTokens.js
@@ -1,15 +1,30 @@
 // grab all token balances and then approve allowance to the relayer
 // have them paste in the private key to approve?
 // then if something happens, you can go manually click on run autotask, which will transfer out all tokens for you
+require('dotenv').config();
 const axios = require('axios');
 const { ethers } = require('ethers');
-const yargs = require('yargs');
-dotenv.config({ path: __dirname + '/.env' });
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COVALENT_API_KEY = process.COVALENT_API_KEY;
+const COVALENT_API_KEY = process.env.COVALENT_API_KEY;
+const yargs = require('yargs');
 
-const erc20Abi = ['function approve(address _spender, uint256 _value) external'];
+const options = {
+  network: {
+    type: 'string',
+    required: true,
+    description: 'network',
+    alias: 'n',
+  },
+};
+
+const cliOptions = yargs.options(options).parseSync();
+
+const erc20Abi = [
+  'function approve(address _spender, uint256 _value) external',
+  'function allowance(address owner, address spender) external view returns (uint256)',
+];
 
 const erc721Abi = ['function approve(address spender, uint256 amount) external returns (bool)'];
 
@@ -24,13 +39,30 @@ async function get(url, method, headers, auth) {
 }
 
 async function main() {
+  const network = cliOptions.network;
+  let rpcEndpoint;
+  let chainId;
+
+  // using arguments from command line to avoid importing values from yaml file (requires rollup.js)
+  if (network === 'goerli') {
+    rpcEndpoint = GOERLI_RPC_URL;
+    chainId = '5';
+  } else if (network === 'mainnet') {
+    rpcEndpoint = MAINNET_RPC_URL;
+    chainId = '1';
+  }
+
+  // get wallet address from private key
+  const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+  const walletAddress = await signer.getAddress();
+
   const method = 'get';
   const headers = {
     'Content-Type': 'application/json',
   };
   // @todo replace chain id and address with args from the command line
-  const url =
-    'https://api.covalenthq.com/v1/5/address/0x7CCc699cb7F361Aa1b982E7954cF65E531cdEc94/balances_v2/?no-nft-fetch=true&nft=true';
+  const url = `https://api.covalenthq.com/v1/${chainId}/address/${walletAddress}/balances_v2/?no-nft-fetch=true&nft=true`;
   const auth = {
     username: COVALENT_API_KEY,
   };
@@ -45,8 +77,7 @@ async function main() {
     console.log('error retrieving wallet balance: ');
     console.log(error);
   }
-  const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL);
-  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+
   let erc20Contract = new ethers.Contract('0x', erc20Abi, signer); // first address doesn't matter, will be replaced with the address of the token
   let erc721Contract = new ethers.Contract('0x', erc721Abi, signer); // first address doesn't matter, will be replaced with the address of the token
 
@@ -60,21 +91,25 @@ async function main() {
     // ignore if token value is less than $0.1
     if (balance > 0 && !isNft && !isDust) {
       let specificErc20Contract = erc20Contract.attach(item.contract_address);
-      // approve all tokens to relayer address
-      // @todo replace relayer-address with args from command line
-      // @todo check if relayer already has allowances to avoid duplicate approvals
-      await specificErc20Contract.approve('relayer-address', balance);
-      console.log('approving token address...', item.contract_address);
+      // check if relayer already has allowances to avoid duplicate approvals
+      let allowance = await specificErc20Contract.allowance(
+        walletAddress,
+        '0x7CCc699cb7F361Aa1b982E7954cF65E531cdEc94',
+      );
+      // only need to approve if allowance is less than balance
+      if (allowance.lt(balance)) {
+        // @todo replace relayer-address with address of defender relayer in your account
+        await specificErc20Contract.approve('0x7CCc699cb7F361Aa1b982E7954cF65E531cdEc94', balance);
+      }
       let scaledBalance = balance / Math.pow(10, decimals);
       scaledBalance = scaledBalance.toFixed(2);
-      console.log(`You have ${scaledBalance} of ${symbol}`);
+      console.log(`Approved allowance of ${scaledBalance} for ${symbol}`);
     } else if (balance > 0 && isNft) {
       // @todo replace relayer-address with args from command line
       let specificErc721Contract = erc721Contract.attach(item.contract_address);
-      await specificErc721Contract.approve('relayer-address', balance);
+      await specificErc721Contract.approve('0x7CCc699cb7F361Aa1b982E7954cF65E531cdEc94', balance);
       console.log(`You have ${balance} of nft ${symbol}`);
     }
-    // include edge case for ether? Because it's not an erc20
   });
 
   await Promise.all(promises);
