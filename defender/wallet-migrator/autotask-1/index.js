c6cdb86b2fdd9a7f026f54bc3d80e848d3a703b5
darian-ae <darian@arbitraryexecution.com>
2023-02-28 11:59:57 -0800

    fixed autotask code from pr comments

--- defender/wallet-migrator/autotask-1/index.js
+++ defender/wallet-migrator/autotask-1/index.js
@@ -2,7 +2,6 @@ const stackName = 'wallet_migrator';
 const senderWalletAddressSecretName = `${stackName}_SENDER_WALLET_ADDRESS`;
 const receiverWalletAddressSecretName = `${stackName}_RECEIVER_WALLET_ADDRESS`;
 const covalentApiKeySecretName = `${stackName}_COVALENT_API_KEY`;
-const networkSecretName = `${stackName}_NETWORK`;
 
 const axios = require('axios');
 const { ethers } = require('ethers');
@@ -61,20 +60,15 @@ exports.handler = async function handler(autotaskEvent) {
     throw new Error('covalent-ap-key must be defined in config.dev.yml file or Defender->AutoTask->Secrets');
   }
 
-  const network = secrets[networkSecretName];
+  // create new provider and signer to perform on-chain transfers
+  console.debug('Creating DefenderRelayProvider');
+  const provider = new DefenderRelayProvider(autotaskEvent);
+  console.debug('Creating DefenderRelaySigner');
+  const signer = new DefenderRelaySigner(autotaskEvent, provider, { speed: 'fast' });
 
-  // ensure that the network exists
-  if (network === undefined) {
-    throw new Error('network must be defined in config.dev.yml file or Defender->AutoTask->Secrets');
-  }
-
-  // get chain id depending on network
-  let chainId;
-  if (network === 'goerli') {
-    chainId = '5';
-  } else if (network === 'mainnet') {
-    chainId = '1';
-  }
+  // get address and chain id
+  const relayerAddress = await signer.getAddress();
+  const chainId = await signer.getChainId();
 
   // query senders wallet balance for all erc20 and erc721 tokens
   const method = 'get';
@@ -97,66 +91,65 @@ exports.handler = async function handler(autotaskEvent) {
     console.log(error);
   }
 
-  // create new provider and signer to perform on-chain transfers
-  console.debug('Creating DefenderRelayProvider');
-  const provider = new DefenderRelayProvider(autotaskEvent);
-  console.debug('Creating DefenderRelaySigner');
-  const signer = new DefenderRelaySigner(autotaskEvent, provider, { speed: 'fast' });
-  const relayerAddress = await signer.getAddress();
-
-  let erc20Contract;
-  let erc721Contract;
-
-  // these initial contract addresses are just placeholders - they will be replaced by each erc20 and erc721 contract address the user holds
-  // it can cause an error with ethers.js if the contract address is invalid, so we are passing in known valid contract addresses
-  // as place holders for erc20 and erc721 tokens on both goerli and mainnet
-  if (network === 'goerli') {
-    erc20Contract = new ethers.Contract('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', erc20Abi, signer);
-    erc721Contract = new ethers.Contract('0xCfD7cEF761A60dFBA0D240ee4fF82f7f51242675', erc721Abi, signer);
-  } else if (network === 'mainnet') {
-    erc20Contract = new ethers.Contract('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', erc20Abi, signer);
-    erc721Contract = new ethers.Contract('0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D', erc721Abi, signer);
-  }
-
   for (var i = 0; i < responseData.length; i++) {
-    const item = responseData[i];
-    const balance = item.balance;
-    const isNft = item.type === 'nft';
-    const isDust = item.type === 'dust'; // tokens with less than $0.1 in spot fiat value get classified as dust (ignore unless it's eth)
-    const decimals = item.contract_decimals;
-    const symbol = item.contract_ticker_symbol;
-    const isEther = item.contract_address === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+    const {
+      balance,
+      type,
+      contract_decimals: decimals,
+      contract_ticker_symbol: symbol,
+      contract_address: contractAddress,
+      nft_data: nftCollectionData,
+    } = responseData[i];
+    const isNft = type === 'nft';
+    const isDust = type === 'dust'; // tokens with less than $0.1 in spot fiat value get classified as dust (ignore unless it's eth)
+    const isEther = contractAddress === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 
     // ignore if token value is less than $0.1 or is Ether
     // address 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee is used to represent Ether
     // we cannot call allowance() or transferFrom() on Ether because it is not an erc20 token
     // there isn't a way to approve an allowance of Ether ahead of time like we can or erc20 tokens
-    if (balance > 0 && !isNft && !isDust && !isEther) {
-      const specificErc20Contract = erc20Contract.attach(item.contract_address);
+    const balanceBigNumber = ethers.BigNumber.from(balance);
+    if (balanceBigNumber.gt(0) && !isNft && !isDust && !isEther) {
+      const specificErc20Contract = new ethers.Contract(contractAddress, erc20Abi, signer);
       // check if relayer has allowance before sending
       const allowance = await specificErc20Contract.allowance(senderWalletAddress, relayerAddress);
-      if (allowance > 0) {
-        const tx = await specificErc20Contract.transferFrom(senderWalletAddress, receiverWalletAddress, allowance);
+      if (allowance.gt(0)) {
+        // send the minimum of either allowance or balance
+        // its possible to approve an allowance for more than your balance
+        let amountToTransfer;
+        if (allowance.gt(balanceBigNumber)) {
+          amountToTransfer = balanceBigNumber;
+        } else {
+          amountToTransfer = allowance;
+        }
+        const tx = await specificErc20Contract.transferFrom(
+          senderWalletAddress,
+          receiverWalletAddress,
+          amountToTransfer,
+        );
         await tx.wait();
-        // scale allowances for logging
-        let scaledAllowance = allowance / Math.pow(10, decimals);
-        scaledAllowance = scaledAllowance.toFixed(2);
+        // scale amount sent for logging
+        const scaledAmount = ethers.utils.formatUnits(amountToTransfer, decimals);
         // scale balances for logging
-        let scaledBalance = balance / Math.pow(10, decimals);
-        scaledBalance = scaledBalance.toFixed(2);
+        const scaledBalance = ethers.utils.formatUnits(balance, decimals);
         // balance and allowance should be the same because the total balance should have been previously approved
         console.log(
-          `Out of ${scaledBalance} ${symbol} tokens in wallet ${senderWalletAddress}, ${scaledAllowance} was sent to wallet ${receiverWalletAddress}`,
+          `Out of ${scaledBalance} ${symbol} tokens in wallet ${senderWalletAddress}, ${scaledAmount} was sent to wallet ${receiverWalletAddress}`,
+        );
+      } else {
+        // ensure there is an allowance for all tokens
+        // otherwise, alert user of error and re-run Autotask after allowances are approved
+        throw new Error(
+          `Relayer address ${relayerAddress} has no allowance for erc20 token at contract address: ${contractAddress}). Approve allowance and re-run Autotask`,
         );
       }
-    } else if (balance > 0 && isNft) {
-      const specificErc721Contract = erc721Contract.attach(item.contract_address);
+    } else if (balanceBigNumber.gt(0) && isNft) {
+      const specificErc721Contract = new ethers.Contract(contractAddress, erc721Abi, signer);
       // check if relayer is approved as an operator before transferring
       // transfer all nfts one by one if relayer is approved as an operator
       // we need to do this because there is no transfer all method for erc721 and a sender may have more than 1 nft in a collection
       const isApprovedForAll = await specificErc721Contract.isApprovedForAll(senderWalletAddress, relayerAddress);
       if (isApprovedForAll) {
-        const nftCollectionData = item.nft_data;
         nftCollectionData.map(async individualNft => {
           const tokenId = individualNft.token_id;
           const tx = await specificErc721Contract.transferFrom(senderWalletAddress, receiverWalletAddress, tokenId);
@@ -165,6 +158,10 @@ exports.handler = async function handler(autotaskEvent) {
             `Transfered NFT ${symbol} with token id ${tokenId} from ${senderWalletAddress} to ${receiverWalletAddress}`,
           );
         });
+      } else {
+        throw new Error(
+          `Relayer address ${relayerAddress} has no allowance for erc721 token at contract address: ${contractAddress}). Approve allowance and re-run Autotask`,
+        );
       }
     }
   }
